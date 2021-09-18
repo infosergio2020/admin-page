baa908996cd3be92fb613389cd79a4b73293f3d8
infosergio2020 <infosergio2020@gmail.com>
2021-09-18 19:50:42 -0300

    sergio quito espacios en blanco y agrego cierres a l algunos tags

--- src/App.js
+++ src/App.js
@@ -6,43 +6,17 @@ import { Header } from "./componentes/header";
 
 
 function App() {
-    // return ( <
-    //     div className = "App" >
-    //     <
-    //     header className = "App-header" >
-    //     <
-    //     img src = { logo }
-    //     className = "App-logo"
-    //     alt = "logo" / >
-    //     <
-    //     p >
-    //     Edit < code > src / App.js < /code> and save to reload. <
-    //     /p> <
-    //     a className = "App-link"
-    //     href = "https://reactjs.org"
-    //     target = "_blank"
-    //     rel = "noopener noreferrer" >
-    //     Learn React <
-    //     /a> <
-    //     /header> <
-    //     /div>
-    // );
-    return ( <
-        div className = "App" >
-        <
-        h1 > Hello CodeSandbox < /h1>  <
-        h2 > Start editing to see some magic happen! < /h2>  {
-            /* <Button> Cerrar sesión</Button> 
-                    
-                        <img src={"./componentes/svg/icono.svg"} /> */
-        }
-
-        <
-        Header > < /Header> {
-            /* 
-                    <Buscador> </Buscador>   */
-        } <
-        /div>
+    return ( 
+        <div className = "App" >
+            <h1> Hello CodeSandbox </h1>
+            <h2> Start editing to see some magic happen! </h2>  
+            {
+                /* <Button> Cerrar sesión</Button> 
+                <img src={"./componentes/svg/icono.svg"} /> */
+            }
+            <Header />
+            {/* <Buscador/>*/}
+        </div>
     );
 }
 
