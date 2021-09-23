87cbbe4f11a8d0bf33b8a187dc9d727005f69a14
ori <ori_larusa@hotmail.com>
2021-09-23 01:11:56 -0300

    se ven los campos

--- src/AdminApp.js
+++ src/AdminApp.js
@@ -2,11 +2,12 @@ import React from 'react'
 import { PopWindow } from './components/general/popWindow/PopWindow';
 import video from './icons/photo.svg';
 
+
 import './main.css'
 
 function AdminApp() {
     return (
-        <div>
+        <div class="tamaño">
             <PopWindow icon={video} title="Nueva imagen"/>
             {/* <div className="hola">
                 <VideoApp url="https://youtu.be/or8QfmradNM" />
--- src/components/general/popWindow/PopWindow.jsx
+++ src/components/general/popWindow/PopWindow.jsx
@@ -4,37 +4,49 @@ import './popWindow.css'
 
 export const PopWindow = ({title,icon,setData}) => {
 
-
-    const [inputValue, setInputValue] = useState('');
+// declaracion de un estado
+    const [datos, setDatos] = useState({
+        titulo:" ",
+        url:" ",
+        descripcion: " "
+    });
 
     const handleInputChange = (e)=>{
-        setInputValue( e.target.value );
+        setDatos( {
+            ...datos, 
+            [e.target.name] : e.target.value
+        });
     }
 
-    const handleSubmit = (e)=>{
-        console.log('hadleSubmit',inputValue);
-        e.preventDefault();        
-        if(inputValue.trim().length > 2 ){
-            setData( cats => [inputValue,...cats]);
-            setInputValue('');
-        }else {
-            setInputValue('');
-        }
-    }
+    // const handleSubmit = (e)=>{
+    //     console.log('hadleSubmit',inputValue);
+    //     e.preventDefault();        
+    //     if(inputValue.trim().length > 2 ){
+    //         setData( cats => [inputValue,...cats]);
+    //         setInputValue('');
+    //     }else {
+    //         setInputValue('');
+    //     }
+    // }
 
     return (
         <>
+        
         <div className="popWindow__header">
             <h2> <img src={icon}/>  {title}</h2>
         </div>
-        <form  className="form background-form" onSubmit={handleSubmit}>
+        {/* <form  className="form background-form" onSubmit={handleSubmit}> */}
+        <form  className="form background-form">
             <h3>Complete los campos a continuación</h3>
-            <p> {inputValue} </p>
+            <p>titulo: {datos.titulo} </p>
+            <p>url: {datos.url} </p>
+            <p>descripcion: {datos.descripcion} </p>
             <div className="input-group">
                 <input 
                     className="input input-background" 
                     type="text"
-                    value={inputValue}
+                    // value={inputValue}
+                    name="titulo"
                     placeholder="Titulo"
                     onChange={handleInputChange}>
                 </input>
@@ -42,12 +54,13 @@ export const PopWindow = ({title,icon,setData}) => {
                 <input 
                     className="input input-background" 
                     type="text"
+                    name="url"
                     placeholder="Url"
-                    value={inputValue}
+                    // value={inputValue}
                     onChange={handleInputChange}>
                 </input>
                 
-                <textarea className="input-textArea input-background" >Descripción</textarea>
+                <textarea className="input-textArea input-background" type="text" name="descripcion" onChange={handleInputChange} placeholder="Descripción"></textarea>
 
             </div>
         </form>
