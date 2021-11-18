8c3fbcd435e2f09a9d314c6b1a8d7e63867b4dd7
infosergio2020 <infosergio2020@gmail.com>
2021-11-18 18:23:46 -0300

    i will have order

--- src/containers/NewArea.jsx
+++ src/containers/NewArea.jsx
@@ -7,27 +7,20 @@ import iconos from '../assets/img/iconos';
 //CSS
 import "../styles/NewArea.css"
 
-export const NewArea = ({setData,area}) => {
+export const NewArea = ({setData}) => {
         const [activeV, setactiveV] = useState(false);
-        
         const [activeF, setactiveF] = useState(false);
-
         const [arrayF, setarrayF] = useState( // para la foto
             []
         );
-        
-        useEffect(()=>{    }, [ arrayF ]);
-
         const [arrayV, setarrayV] = useState( // para el video
             []
         );
-        
         useEffect(()=>{    }, [ arrayV ]);
-// Nota: Tuve que crear 2 estados separados para no eliminar 
-// o cambiar la estrucutra del POPWindow que en ese
-// mismo componente estan video y foto---> es una mierda
-
-// declaracion de un estado area para video
+        // Nota: Tuve que crear 2 estados separados para no eliminar 
+        // o cambiar la estrucutra del POPWindow que en ese
+        // mismo componente estan video y foto---> es una mierda
+        // declaracion de un estado area para video
         const [datos, setDatos] = useState({
             nombreArea:" ",
             descripcionArea: " ",
@@ -43,28 +36,15 @@ export const NewArea = ({setData,area}) => {
         setDatos( {
             ...datos, 
             [e.target.name] : e.target.value
-            
-        });
-    }
-
-    const [archivos, setArchivos] = useState(
-        {url:" "}
-    );
-// crear funcion para almacenar las imagenes
- 
-    const subirArchivos = (l)=>{
-        setArchivos( {
-            ...archivos, 
-            [l.target.name] : l.target.value
         });
     }
 // me guardo los datos que se escribieron en el NewArea
 function guardarDatos(e) {
+    e.preventDefault(); // la pagina no se regargue
     localStorage.setItem('datos', JSON.stringify(datos));// para el video
     //localStorage.setItem('datosF', JSON.stringify(datos)); // para la foto
     guardarFoto();
     guardarVideo();
-    e.preventDefault(); // la pagina no se regargue
     console.log(e); // que evento nos tira
 }
 
@@ -72,15 +52,12 @@ function guardarFoto(){
     localStorage.setItem('ListaFotos', JSON.stringify(arrayF));
     console.log("guardar")
     // verImagen()
-    
 }
 
 function guardarVideo(){
     localStorage.setItem('ListaVideos', JSON.stringify(arrayV));
-    
 }
 
- 
 // funcion para simular el POPUP del video y la foto
 function popApV(e){
     e.preventDefault()
@@ -92,34 +69,30 @@ function popApF(e){
 } 
 
 // function verImagen(){
-    
 //     console.log("hola")
-    
-
-
 //     const area = JSON.parse( localStorage.getItem('ListaFotos'));
 //     let resultado=area;
 //     console.log(resultado[0].src)
 //     var preview = document.getElementById('hola');
-    
 //         preview.url = resultado[0].url
-    
 //     if (area === null){
 //         resultado="Sin informacion";
 //     }
 //     //return resultado;
 // }
 
-
 // COMIENZO DEL MAIN()
     return (
         <>
+            {/* LISTADO DE ELEMENTOS DE UN ARRAY */}
+            {/* <ul>
+                { arrayV.map( unVideo => { <li>{unVideo}</li> }) }
+            </ul> */}
+
         <div className="container-Switch">
             <div className="flex bg-gray">
             {/* aca estaba el form */}
-                
                     {/* mostrar y esconder los campos del primer div de AREA */}
-                    {area && 
                     <form  className="form background-form">
                             <h3 tabIndex="0" aria-describedby="Complete los campos a continuación" >Complete los campos a continuación.</h3>
                             {/* <p>nombre: {datos.nombre} </p>
@@ -142,74 +115,54 @@ function popApF(e){
                                     </div>
                                     <div className="">
                                         <div className="horizontalBotones">
-                                        <Boton buttonStyle="azul" icono={iconos.coord}> Añadir Posicion </Boton>
-                                        
-                                        <Boton buttonStyle="azul" icono={iconos.play}> Añadir juego </Boton> 
-
-
+                                            <Boton buttonStyle="azul" icono={iconos.coord}> Añadir Posicion </Boton>
+                                            <Boton buttonStyle="azul" icono={iconos.play}> Añadir juego </Boton> 
                                         </div>
+
                                         <div className="horizontalBotones">
-                                        <Boton buttonStyle="azul" icono={iconos.video} onClick={popApV}> Añadir video </Boton>
-                                        
-                                        {/* boton añadir imagen */}
-                                                
-                                        <label className="label" for="cambio">
-                                                    <p>Añadir imagen</p> 
-                                                    <img src={iconos.photo} />
-                                        </label>                                   
-                                                    <input 
-                                                    
-                                                        id="cambio"                                    
-                                                        type="file"
-                                                        name="urlArea"
-                                                        placeholder="Añadir imagen"                                                                                                              
-                                                        // value={inputValue}
-                                                        accept="image/gif, image/png, image/peg, "
-                                                        multiple onChange={()=>subirArchivos} onClick={popApF}>
-                                                    </input>
-                
-                                        </div>
-                                            
+                                            <Boton buttonStyle="azul" icono={iconos.video} onClick={popApV}> Añadir video </Boton>
+                                            {/* boton añadir imagen */}
+                                            <label className="label" for="cambio">
+                                                        <p>Añadir imagen</p> 
+                                                        <img src={iconos.photo} />
+                                            </label>                                   
+                                            <input 
+                                                id="cambio"                                    
+                                                type="file"
+                                                name="urlArea"
+                                                placeholder="Añadir imagen"                                                                                                              
+                                                // value={inputValue}
+                                                accept="image/gif, image/png, image/peg, "
+                                                onClick={popApF}>
+                                            </input>
+                                        </div>                             
                                     </div>                        
-                                    
                                 </div>
                                 <textarea  className="input-textArea input-background" type="text" name="descripcionArea" onChange={handleInputChange} placeholder="Descripción" aria-multiline="true"></textarea>
-
-                            
                             </div>  
                             <div className="center">
-
                                 <Boton buttonStyle="verde" icono={iconos.check} onClick={guardarDatos}> Guardar </Boton>
                                 <Boton buttonStyle="rojo" icono={iconos.cancel}> Cancelar </Boton>
                                 {/* <br></br>
                                 <img src="" id="hola" height="200" alt="Image preview..."></img> */}
-                            </div>
-                            
+                            </div> 
                 </form>             
-                
-                    }       
+
             </div>
+            {/* ACA SE ESCONDEN O VISUALIZAN POPUPS */}
+            {/* VIDEO */}
             <div className={activeV ? 'pop-display pop-display-active' : 'pop-display'}>
                 <div className="card-popup">
                     <PopWindow title="video" icon={iconos.video} video={true} setarray={setarrayV} esconder={setactiveV}> </PopWindow>
                 </div>
-                
             </div>
+            {/* FOTO */}
             <div className={activeF ? 'pop-display pop-display-active' : 'pop-display'}>
                 <div className="card-popup">
                     <PopWindow title="imagen" icon={iconos.photo} video={false} setarray={setarrayF} esconder={setactiveF}> </PopWindow>
                 </div>
-                
             </div>
         </div>
-        </>
-        
-      
+        </> 
     )
-
-}
-
-// con esto prevengo que alguien que reutilize mi funcion se olvide de mandar una funcion como parametro
-NewArea.propTypes = { 
-    setData: PropTypes.func.isRequired,
 }
