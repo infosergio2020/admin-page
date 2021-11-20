2cc4e8e1545efb96c14bfd69425ea45570b3b06b
lau-yt <laura.y.lp96@gmail.com>
2021-11-20 17:37:00 -0300

    cambios en la url funciona
    En el arreglo la url cambia de manera correcta

--- src/components/PopWindow.jsx
+++ src/components/PopWindow.jsx
@@ -7,7 +7,7 @@ import '../styles/popWindow.css'
 //ICONOS
 import iconos from '../../src/assets/img/iconos';
 
-export const PopWindow = ({title,icon,setData,video, setDatosArea, hidePop }) => {
+export const PopWindow = ({title,icon,setData,video, setarray , esconder}) => {
 // declaracion de un estado
         const [datos, setDatos] = useState({
             titulo:" ",
@@ -15,6 +15,7 @@ export const PopWindow = ({title,icon,setData,video, setDatosArea, hidePop }) =>
             descripcion: " "
         });
 
+      
 // crear funcion para ver los inputs
     const handleInputChange = (e)=>{
         //console.log(e.target.value);
@@ -22,24 +23,39 @@ export const PopWindow = ({title,icon,setData,video, setDatosArea, hidePop }) =>
             ...datos, 
             [e.target.name] : e.target.value
         });
+        
     }
 
-    // const guardar = (e)=>{
-    //     setDatosArea( {
-    //         ...datos, 
-    //         datos        
-               
-    //     });
-    //     // revisar si esta parte funciona--> Oriana dice: NO ES NECESARIO TENERLO PORQUE PODEMOS ACCEDER
-    // DESDE CUALQUIER ARCHIVO MEDIANTE EL NOMBRE DE LA CLAVE DEL LOCALSTORAGE
-    // SOY RE CAPA, ME HABIA OLVIDADO DE ESO Y RECIEN ME ACUERDO JE
-    // Nota: tengo la necesidad de separar video de foto, porque se van a pisar 
-    // }
 
     // guardado del LocalStorage
-        function guardar(){
-            localStorage.setItem('datos', JSON.stringify(datos));
+        function guardar(e){
+            //localStorage.setItem('datosF', JSON.stringify(datos));
+            //insertar elemento en un array
+            e.preventDefault();
+            
+            // if(!video){
+            //     var preview = document.querySelector('img');
+            //     var file    = document.querySelector('input[type=file]').files[0];
+            //     var reader  = new FileReader();
+                
+            //     reader.onloadend = function () {
+            //     preview.src = reader.result;
+            //     }
+            
+            //     if (file) {
+            //         reader.readAsDataURL(file);
+            //         console.log(preview.src)
+            //     } else {
+            //         preview.src = "";
+            //     }                
+            // }  
+            setarray(array=>
+                [...array,datos]  
+            );    
+            esconder(esconder=>!esconder);
+            
         }
+// Funciones
 
 const previewFile = ((e) => {
     var preview = document.getElementById('hola');
@@ -53,13 +69,6 @@ const previewFile = ((e) => {
         preview.src = reader.result;
     //   datos.url=reader.result;   //Todo esto lo tendria que usar cuando guarde todo el evento
     }
-    // const insertarArchivos= async()=>{
-    //     const f=new FormData();
-
-    //     for(let i=0; i<archivos.lenght;i++){
-    //         f.append("files",archivos[i]);
-    //     }
-    // }
 
     if (file) {
         reader.readAsDataURL(file); 
@@ -74,15 +83,10 @@ const previewFile = ((e) => {
         
         <div className="popWindow__header">
             <h2 tabIndex="0" aria-label={title}> <img alt="imagen. icono de imagen." src={icon}/>  {title}</h2>
-            <Boton buttonStyle={"btn-small-circle blanco"} icono={iconos.close} onClick={hidePop}></Boton>
         </div>
         {/* <form  className="form background-form" onSubmit={handleSubmit}> */}
-        <form  className="form background-form">
+        <form  name="formularioUI" className="form background-form">
             <h3 tabIndex="0" aria-describedby="Complete los campos a continuación" >Complete los campos a continuación.</h3>
-            <p>titulo: {datos.titulo} </p>
-            <p>url: {datos.url} </p>
-            <p>descripcion: {datos.descripcion} </p>
-            <p>archivos: {archivos.url} </p>
             <div className="input-group">
                 <input 
                     className="input input-background" 
@@ -108,25 +112,23 @@ const previewFile = ((e) => {
 
                 {/* mostrar y esconder el campo cargar imagen */}
                 {!video && 
+
                 <div className="center">
-                
                 <input 
                     className="" 
                     type="file"
                     name="url"
-                    placeholder="Subir archivo"
-                    
-                    
-                
+                    id= "lol"
+                    placeholder="Subir archivo"              
                     // value={inputValue}
                     accept="image/gif, image/png, image/peg, "
                     //src={window.location.origin} //si lo coloco aca me da el host
                     onChange={previewFile}
                     >
                 </input>
-                </div>
-            
-                
+                <br></br>
+                <img src="" id="hola" height="200" alt="Image preview..."></img>
+                </div>           
                 }
 
                 <textarea className="input-textArea input-background" type="text" name="descripcion" onChange={handleInputChange} placeholder="Descripción" aria-multiline="true"></textarea>
@@ -143,7 +145,7 @@ const previewFile = ((e) => {
             {!video &&
                 <div className="center">
 
-                    <Boton buttonStyle="verde" icono={iconos.check} onClick={guardarF}> GuardarFoto </Boton>
+                    <Boton buttonStyle="verde" icono={iconos.check} onClick={guardar}> GuardarFoto </Boton>
                     <Boton buttonStyle="rojo" icono={iconos.cancel}> CancelarFOTO </Boton>
                 </div>
             }
