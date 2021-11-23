12c769262115466b33dccf503e24f0233708b311
ori <ori_larusa@hotmail.com>
2021-11-23 16:26:08 -0300

    estoy tratando de arregalr el llamadado a las vistas en el menu

--- src/pages/VerDetalleEvento.jsx
+++ src/pages/VerDetalleEvento.jsx
@@ -1,36 +1,73 @@
-import React from 'react'
+import React, { useState} from 'react';
 import '../styles/VerDetalleEvento.css'
 import { VerArea } from "./VerArea";
 import { ViewMultimedia } from "../containers/ViewMultimedia";
 
-const ListAreas = ({items}) =>{
-    return (
-        <ul className="listAreasDetailsEvent-list">
-            {items.map(item =>  <li className="listAreasDetailsEvent-item">{item}</li>)}
-        </ul>
-    )
-}
+
+
+    
 
 
 
 export const VerDetalleEvento = ({areas=["Area-1","Area-2","Area-3","Area-4"]}) => {
-    const flagArea = false;
+    const [mostrar, setmostrarM] = useState(false);
+    const [mostrarA, setmostrarA] = useState(false);
+
+    function activarMostrarMultimedia(e){
+        setmostrarM(!mostrar);
+    } 
+    function activarMostrarArea(e){
+        setmostrarA(!mostrarA);
+    } 
+    const ListAreas = ({items}) =>{
+        return (
+            <ul className="listAreasDetailsEvent-list">
+                {items.map(item =>  <li className="listAreasDetailsEvent-item" onClick={activarMostrarArea}>{item}</li>)}
+            </ul>
+        )
+    }
+    
+    // FUNCION
+    function acordion(){
+        var acc = document.getElementsByClassName("accordion");
+        var i;
+        
+        for (i = 0; i < acc.length; i++) {
+          acc[i].addEventListener("click", function() {
+            this.classList.toggle("active");
+            var panel = this.nextElementSibling;
+            if (panel.style.maxHeight) {
+              panel.style.maxHeight = null;
+            } else {
+              panel.style.maxHeight = panel.scrollHeight + "px";
+            } 
+          });
+        }
+    }
+    
+//MAIN
+
+
     return (
         <div className="detalle-container">
         {/* botones para pestañas */}
             <section className="detalle-sideA">
                 <article>
-                    <h3>Multimedia</h3>
-                    <h3>Areas</h3>
-                    <ListAreas items={areas}/>
+                  
+                    <button className="listAreasDetailsEvent-item" onClick={activarMostrarMultimedia}><h3>Multimedia</h3></button>
+                    <button className="accordion listAreasDetailsEvent-item" onClick={acordion()}><h3>Areas</h3></button>
+                        <div className="panel">
+                           <ListAreas items={areas}/>
+                        </div>
+                    
                 </article>
             </section>
 
         {/* pestañas */}
             <section className="detalle-sideB">
 
-                {flagArea && <VerArea />}
-                {!flagArea && <ViewMultimedia />}
+                {mostrarA && <VerArea />}
+                {mostrar && <ViewMultimedia />}
                 
             </section>
         </div>
