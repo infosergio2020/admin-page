2d09abdc98ea370b84976eec9249da326dfbf2cf
infosergio2020 <infosergio2020@gmail.com>
2022-01-06 13:42:46 -0300

    rama de prueba para la pwa aun que estoy deberia ser de la app, no del admin xD

--- src/index.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './styles/main.css';
-import { AdminApp } from "./routes/AdminApp";
-ReactDOM.render( <AdminApp />, document.getElementById('root') );
