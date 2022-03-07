5ac51eb142203c0a0c689cb1c0a8b6e522767aa4
infosergio2020 <infosergio2020@gmail.com>
2022-03-07 12:49:05 -0300

    clean

--- src/index.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './main.css';
-import { AdminApp } from "./AdminApp";
-ReactDOM.render( <AdminApp />, document.getElementById('root') );
